52211ef3a4b81808400475c0d027eba4f73be6e5
codewithmayor <mayor.michael19@gmail.com>
2023-02-13 07:15:36 +0200

    models

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
-"""__init__magic method for models directory"""
-from models.engine.file_storage import FileStorage
+from .engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.city import City
+from models.review import Review
+from models.state import State
+from models.amenity import Amenity
 
+classes = {
+    "BaseModel": BaseModel,
+    "User": User,
+    "City": City,
+    "Place": Place,
+    "Review": Review,
+    "State": State,
+    "Amenity": Amenity
+}
 
 storage = FileStorage()
 storage.reload()
